efe1edb1f391066906cca1a2ff29e36fa2990ca8
Ben Krieger <blitzrk@gmail.com>
2016-05-03 12:28:43 -0600

    Update test structure

--- tests/subl_mock.py
+++ tests/subl_mock.py
@@ -2,17 +2,15 @@ from functools import wraps
 import os
 from sublime import version
 
-def subl_patch(pkg, obj=None):
+def subl_patch(pkg, obj=''):
     def subl_deco(fn):
         @wraps(fn)
         def wrap(*args):
             repo = os.path.realpath(__file__).split(os.sep)[-3]
-            wrap.pkg = pkg if version() < '3000' else repo
-            mock = __import__(wrap.pkg)
-            if version() >= '3000':
-                mock = getattr(mock, pkg)
-            if obj is not None:
-                mock = getattr(mock, obj)
+            wrap.obj = [obj] if obj != '' else []
+            wrap.pkg = pkg if version() < '3000' else repo+'.'+pkg
+            mock = __import__(wrap.pkg, None, None, wrap.obj)
+            mock = getattr(mock, obj, mock)
             args += (mock,)
             fn(*args)
         return wrap
--- tests/test_pathutils.py
+++ tests/test_pathutils.py
@@ -4,14 +4,14 @@ import sublime
 from unittest import TestCase
 
 class TestPathutils(TestCase):
-    @subl_patch('libsass', 'pathutils')
+    @subl_patch('lib', 'pathutils')
     def test_subpaths(self, pathutils):
         path = join('/foo','bar','baz')
         exprmt = pathutils.subpaths(path)
         expect = [ join('/foo','bar','baz'), join('/foo','bar'), join('/foo'), join('/') ]
         self.assertEqual(exprmt, expect)
 
-    @subl_patch('libsass', 'pathutils')
+    @subl_patch('lib', 'pathutils')
     def test_grep_r(self, pathutils):
         pathutils.os.walk = lambda x: [('/tmp','',['file.scss'])]
 
